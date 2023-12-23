add69225a29ecfb04ce045e0c7e257ad3c403a46
PHILIP OBIORAH <philip.obiorah@outlook.com>
2023-12-23 21:52:31 +0000

    Added match operation

--- main.py
+++ main.py
@@ -2,11 +2,35 @@
 def add(x, y):
     return x + y
 
+def subtract(x, y):
+    return x - y
 
+def divide(x , y):
+    return x/y
 
+def multipy(x, y):
+    return x*y
 
-val1 = int(input("Enter first value: "))
-val2 = int(input("Enter second value "))
+result = 0
+while True:
+    try:
+        val1 = float(input("Enter first value: "))
 
+        val2 = float(input("Enter second value "))
+        opperation = input("Enter operation( add, subtract, multipy, divide :)")
 
-print(add(val1, val2))
+        match opperation:
+            case "add":
+                result = add(val1,val2)
+
+            case "subtract":
+                result = subtract(val1, val2)  
+
+        print(result)       
+    except ValueError:
+        print("Invalid input. Please enter a number. ")
+    except ZeroDivisionError:
+        print("Cannot devide by zero.")
+
+    if input("Do you want to continue ?(yes/no:) ") != "yes":
+        break
